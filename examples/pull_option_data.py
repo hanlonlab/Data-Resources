59da40383c76f115bc3ced2ea45ee2301587fd5b
Ed <>
2022-10-18 16:56:07 -0400

    Add test print statement

--- examples/pull_option_data.py
+++ examples/pull_option_data.py
@@ -40,7 +40,7 @@ def filter_option_tickers(settings: dict = OPT_PARAMS, day_filter: int = MAX_DAY
     px_last_vals = blp.bds(tickers=settings['tickers'], flds=['PX_LAST'])
     
     for ticker in settings['tickers']:
-        print(px_last_vals.loc[df.columns[0] == ticker])
+        print(px_last_vals.loc[px_last_vals.columns[0] == ticker])
 
         #last_price = float(px_last_vals[px_last_vals.columns[0]])
         
