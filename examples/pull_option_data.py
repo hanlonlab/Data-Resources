e94278a6f579a3d9ac4bd98924a894348cb9c8c0
Ed <>
2022-10-18 16:38:31 -0400

    Add test print statement

--- examples/pull_option_data.py
+++ examples/pull_option_data.py
@@ -39,7 +39,7 @@ def filter_option_tickers(settings: dict = OPT_PARAMS, day_filter: int = MAX_DAY
     px_last_vals = blp.bds(tickers=settings['tickers'], flds=['PX_LAST'])
     
     for ticker in settings['tickers']:
-        print(px_last_vals.loc(px_last_vals[0] == ticker))
+        print(px_last_vals.loc(px_last_vals.iloc[0] == ticker))
         print(blp.bds(tickers=settings['tickers'], flds=['OPT_CHAIN']))
 
 
